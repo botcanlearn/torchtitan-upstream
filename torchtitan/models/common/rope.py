pytorch/torchtitan

[graph_trainer] Support hinted symbolic input dims in tracing
Add the graph_trainer tracing prerequisites needed by later EP chunking work, without introducing the chunking pass itself. minimal_fx_tracer now accepts both mark_unbacked and mark_dynamic metadata on plain tensor inputs, builds the corresponding symbolic context during fakeification, and keeps wrapper-subclass inputs rejected so DTensor-style layouts do not silently lose their metadata.

Make RoPE shape checks symbolic-shape friendly by replacing Python shape asserts with compact torch._check-based validation. This keeps the runtime checks but avoids specializing symbolic batch or sequence dimensions during tracing.

Make full-Inductor FX-to-FX canonicalization tolerate fresh intermediate unbacked scalar symbols, and keep MoE split-size CPU copies synchronous under compile/non-strict tracing so traced split sizes cannot race stale CPU reads.

Test Plan:
- pytest -q torchtitan/experiments/graph_trainer/tests/test_trace_module.py -k 'mark_dynamic_batch_and_seq_dims_with_rope or dtensor_mark_unbacked_rejected'

stack-info: PR: https://github.com/pytorch/torchtitan/pull/3362, branch: sanketpurandare/stack/16

torchtitan/models/common/rope.py
@@ -21,6 +21,16 @@ __all__ = [
 ]
 
 
+def _check_shape_equal(actual, expected, context: str) -> None:
+    for i, (actual_dim, expected_dim) in enumerate(zip(actual, expected, strict=True)):
+        torch._check(
+            actual_dim == expected_dim,
+            lambda i=i, actual_dim=actual_dim, expected_dim=expected_dim: (
+                f"{context} dim {i}: expected {expected_dim}, got {actual_dim}"
+            ),
+        )
+
+
 class RoPE(Module):
     """Shared Rotary Position Embedding module.
 
@@ -224,17 +234,17 @@ def _reshape_for_broadcast_complex(
     seqlen = x.shape[1]
     if positions is None:
         freqs_cis = freqs_cis[0:seqlen]
-        assert freqs_cis.shape == (seqlen, x.shape[-1])
+        _check_shape_equal(freqs_cis.shape, (seqlen, x.shape[-1]), "freqs_cis")
         shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
         return freqs_cis.view(*shape)
     elif positions.size(0) == 1:
-        assert positions.shape == (1, seqlen)
+        _check_shape_equal(positions.shape, (1, seqlen), "positions")
         freqs_cis = freqs_cis[positions.squeeze(0)]
-        assert freqs_cis.shape == (seqlen, x.shape[-1])
+        _check_shape_equal(freqs_cis.shape, (seqlen, x.shape[-1]), "freqs_cis")
         shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
         return freqs_cis.view(*shape)
     else:
-        assert positions.shape == (x.shape[0], seqlen)
+        _check_shape_equal(positions.shape, (x.shape[0], seqlen), "positions")
         freqs_cis_expanded = freqs_cis[None, :, None, :].expand(x.shape[0], -1, -1, -1)
         freqs_cis = torch.gather(
             freqs_cis_expanded,
@@ -261,24 +271,26 @@ def _reshape_for_broadcast_cos_sin(
     bz, seqlen, _, head_dim = x.shape
     if positions is None:
         rope_cache = rope_cache[0:seqlen]
-        assert rope_cache.shape == (seqlen, head_dim * 2)
+        _check_shape_equal(rope_cache.shape, (seqlen, head_dim * 2), "rope_cache")
         shape = [-1, seqlen, 1, head_dim * 2]
         return rope_cache.view(*shape)
     elif positions.size(0) == 1:
-        assert positions.shape == (1, seqlen)
+        _check_shape_equal(positions.shape, (1, seqlen), "positions")
         rope_cache = rope_cache[positions.squeeze(0)]
-        assert rope_cache.shape == (seqlen, head_dim * 2)
+        _check_shape_equal(rope_cache.shape, (seqlen, head_dim * 2), "rope_cache")
         shape = [-1, seqlen, 1, head_dim * 2]
         return rope_cache.view(*shape)
     else:
-        assert positions.shape == (bz, seqlen)
+        _check_shape_equal(positions.shape, (bz, seqlen), "positions")
         rope_cache_expanded = rope_cache[None, :, None, :].expand(bz, -1, -1, -1)
         rope_cache = torch.gather(
             rope_cache_expanded,
             dim=1,
             index=positions.view(bz, seqlen, 1, 1).expand(bz, seqlen, 1, head_dim * 2),
         )
-        assert rope_cache.shape == (bz, seqlen, 1, head_dim * 2)
+        _check_shape_equal(
+            rope_cache.shape, (bz, seqlen, 1, head_dim * 2), "rope_cache"
+        )
         return rope_cache
 
 

botcanlearn/torchtitan-upstream

torchtitan/models/common/rope.py
@@ -36,6 +36,16 @@ def _maybe_check_max_pos(positions: torch.Tensor, *, max_valid_pos: int) -> None
     )
 
 
+def _check_shape_equal(actual, expected, context: str) -> None:
+    for i, (actual_dim, expected_dim) in enumerate(zip(actual, expected, strict=True)):
+        torch._check(
+            actual_dim == expected_dim,
+            lambda i=i, actual_dim=actual_dim, expected_dim=expected_dim: (
+                f"{context} dim {i}: expected {expected_dim}, got {actual_dim}"
+            ),
+        )
+
+
 class RoPE(Module):
     """Shared Rotary Position Embedding module.
 
@@ -362,28 +372,31 @@ def _reshape_for_broadcast(
     cache_width = rope_cache.shape[-1]
     if positions is None:
         rope_cache = rope_cache[0:seqlen]
-        assert rope_cache.shape == (seqlen, cache_width)
+        _check_shape_equal(rope_cache.shape, (seqlen, cache_width), "rope_cache")
         shape = [
             d if i == 1 else cache_width if i == ndim - 1 else 1
             for i, d in enumerate(query_shape)
         ]
         return rope_cache.view(*shape)
     elif positions.size(0) == 1:
-        assert positions.shape == (1, seqlen)
+        _check_shape_equal(positions.shape, (1, seqlen), "positions")
         rope_cache = rope_cache[positions.squeeze(0)]
-        assert rope_cache.shape == (seqlen, cache_width)
+        _check_shape_equal(rope_cache.shape, (seqlen, cache_width), "rope_cache")
         shape = [
             d if i == 1 else cache_width if i == ndim - 1 else 1
             for i, d in enumerate(query_shape)
         ]
         return rope_cache.view(*shape)
     else:
-        assert positions.shape == (bsz, seqlen)
+        _check_shape_equal(positions.shape, (bsz, seqlen), "positions")
         rope_cache_expanded = rope_cache[None, :, None, :].expand(bsz, -1, -1, -1)
         rope_cache = torch.gather(
             rope_cache_expanded,
             dim=1,
             index=positions.view(bsz, seqlen, 1, 1).expand(bsz, seqlen, 1, cache_width),
+        )
+        _check_shape_equal(
+            rope_cache.shape, (bsz, seqlen, 1, cache_width), "rope_cache"
         )
         return rope_cache
 